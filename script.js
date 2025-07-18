18f0feaaab313811901e595d8433d973f87e7dc4
heavenlytalisman <173621005+heavenlytalisman@users.noreply.github.com>
2025-07-18 17:29:15 +0530

    Add: added new grid button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("contextmenu", e => e.preventDefault())
 
 const container = document.getElementById("grid-container");
+const button = document.getElementById("create-grid-button");
 let isLeftClicking = false;
 
 document.addEventListener("mousedown", e => {
@@ -15,23 +16,46 @@ document.addEventListener("mouseup", e => {
     }
 });
 
+function createNewGrid(size) {
+    container.innerHTML = "";
 
-for (let i = 0; i < 16 * 16; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
+    for (let row = 0; row < size; row++) {
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
 
-    square.addEventListener("mouseover", e => {
-        if (isLeftClicking) {
-            square.classList.add("hovered");
-            square.classList.remove("rightClickHovered");
-        } else {
-            square.classList.add("rightClickHovered");
-            square.classList.remove("hovered");
-        }
-    });
+        for(let col = 0; col < size; col++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
 
-    container.appendChild(square);
+            square.addEventListener("mouseover", e => {
+                if (isLeftClicking) {
+                    square.classList.add("hovered");
+                    square.classList.remove("leftClickHovered");
+                } else {
+                    square.classList.add("leftClickHovered");
+                    square.classList.remove("hovered");
+                }
+            
+        });
+        
+        rowDiv.appendChild(square);
+        
+    }
+    
+    container.appendChild(rowDiv);
+    }
 
-};
+}
 
+createNewGrid(16);
+
+button.addEventListener("click", () => {
+    let size = prompt("Enter the number of squares: ");
+    size = parseInt(size);
+    if (isNaN(size) || size > 100 || size < 0) {
+        alert("Invalid number!");
+    } else {
+        createNewGrid(size);
+    }
+});
 
